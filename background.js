b126c5f4257f6a9342fafd4118aaa5a30bf91081
siam5ell <jishnusingh1304@gmail.com>
2026-07-20 16:12:29 +0530

    Updation with Code for safari browser

--- background.js
+++ background.js
@@ -1,14 +1,22 @@
 const html1 = chrome.runtime.getURL("site_blocked_page.html");
 
 function checkbad(s) {
-  const badlist = ["porn", "xhamster", "hentai","youtube.com/shorts"];
+  const badlist = ["youtube.com/shorts"];
   for (let index = 0; index < badlist.length; index++) {
     if (s.includes(badlist[index])) return true;
   }
   return false;
 }
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+chrome.tabs.onUpdated.addListener((_, changeInfo, tab) => {
   if (changeInfo.status === "complete" && checkbad(tab.url.toLowerCase())) {
     chrome.tabs.update({ url: html1 });
+  }
+});
+
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  const url =
+    checkbad(tab.url.toLowerCase()) || checkbad(changeInfo.url.toLowerCase());
+  if (url) {
+    browser.tabs.update(tabId, { url: html1 });
   }
 });
